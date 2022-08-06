a06b248c6c5cb78ae72c5ca4808cf39f103aa137
root <root@DESKTOP-S1F1DJL.localdomain>
2022-08-06 11:20:27 +0700

    06.08.2022

--- main.cpp
+++ main.cpp
@@ -1,34 +1,55 @@
+/**
+ * @file main.cpp
+ * @author konovdan
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <iostream>
 #include <cstring>
 
-struct word
+#define MAX_WORDS 100
+
+// Globals
+//>------------------------------------------------------------------------------------------------
+struct Word
 {
     int index;
     char * enWord;
     char * ruWord;
 };
 
+struct Word words[MAX_WORDS];
+
+// Predefinitions
+//>------------------------------------------------------------------------------------------------
 int get_word_amount(FILE * file);
 std::string get_line(FILE * file);
 void get_words(FILE * file);
 
+
+// Main logic
+//>------------------------------------------------------------------------------------------------
 int main(int argc, char * argv[])
 {
-    argc = 2;
+    argc = 2;                                                                                      // Temporary
+
     if (argc != 2)
     {
         printf("Usage: ./main wordlist.txt\n");
         return 1;
     }
 
-    FILE * file = fopen("words.txt","r");
+    FILE * file = fopen("words.txt","r");                                                          // Temporary
     if (file == NULL)
     {
         printf("Unable to open \"words\" file\n");
         return 2;
     }
-
-    // std::cout << get_line(file) << std::endl;
     
     get_words(file);
 
@@ -36,6 +57,8 @@ int main(int argc, char * argv[])
 }
 
 
+// Helping function
+//>------------------------------------------------------------------------------------------------
 int get_word_amount(FILE * file)
 {
     char ch = 0;
@@ -47,7 +70,6 @@ int get_word_amount(FILE * file)
     };
     return value;
 };
-
 
 std::string get_line(FILE * file)
 {
@@ -63,13 +85,43 @@ std::string get_line(FILE * file)
     return value;
 };
 
-void get_words(FILE * file)
+void get_word(std::string line, struct Word word)                                                  // TODO
 {
-    int amount = get_word_amount(file);
-    
+    std::string value = "";
+
+    int i = 0;
+    while (i != 2)
+    {
+        char ch = fgetc(file);
+        if (ch == ';')
+        {
+            i++;
+            switch (i)
+            {
+            case 1:
+                word.enWord = value;
+                value = "";
+                break;
+            
+            case 2:
+                word.ruWord = value;
+                break;
+            }
+        }
+        else value = value + ch;
+
+    }
+};
+
+void get_words(FILE * file)                                                                        // TODO
+
+{
+    int amount = get_word_amount(file); 
+
+    // Reads file line by line
     for (int i = 0; i < amount; i++)
     {
-        if (i > 10) break;
-        std::cout<< get_line(file) << '\n';
+        get_word(get_line(file), words[i]);
     }
 }
+
