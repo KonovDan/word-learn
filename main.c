2f788b795a8452776df733876b1746128d88cb40
user <user@srv.faked>
2022-12-09 22:31:57 +0700

    09.12.2022

--- main.c
+++ main.c
@@ -12,7 +12,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <locale.h>
 
 #define MAX_WORDS 100
 
@@ -31,76 +30,80 @@ struct Word *wordListPtr;
 
 // Predefinitions
 //>------------------------------------------------------------------------------------------------
-Word * load_words(Word * wordListPtr, FILE * filePtr);
-int get_word_amount(FILE * filePtr);
+FILE * open_file(char * path) ;
+int get_word_amount(FILE * file);
+Word * load_words(char * path);
 
 // Main logic
 //>------------------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
+	Word * list = load_words("words.txt");
 
-
-    FILE * filePtr = fopen("words.txt", "r"); // Temporary
-    if (filePtr == NULL)
-    {
-        printf("Unable to open \"words\" file\n");
-        return 1;
-    }
-
-    Word * listOfWords = load_words(NULL,filePtr);
-
-
-    return 0;
+    	exit(0);
 }
 
 // Helping function
 //>------------------------------------------------------------------------------------------------
-
-int get_word_amount(FILE * filePtr)
+FILE * open_file(char * path)
 {
-    setlocale(LC_ALL, "ru_RU.UTF-16");
+	FILE * filePtr = fopen(path, "r");
+    	if (filePtr == NULL)
+    	{
+        	printf("Unable to open \"words\" file\n");
+        	exit(1);
+    	}
+	return filePtr;
+}
 
+
+
+int get_word_amount(FILE * file)
+{
     char ch = 0;
     int value = 0;
     while (ch + 48 != '\n')
     {
         value = value * 10 + ch;
-        ch = fgetc(filePtr) - 48;
+        ch = fgetc(file) - 48;
     };
     return value;
 };
 
-Word * load_words(Word * wordListPtr, FILE * filePtr)
+Word * load_words(char * path)
 {
-    int wordAmount = get_word_amount(filePtr);
+    	int wordAmount = get_word_amount(open_file(path));
+	Word * wordList = NULL;
+	FILE * file = open_file("words.txt");
 
-    for (int n = 0; n < wordAmount; n++)
-    {
-        char word_1[50] = {0};
-        char word_2[50] = {0};
+    	for (int n = 0; n < wordAmount / 2; n++)
+    	{
+        	char word_1[50] = {0};
+        	char word_2[50] = {0};
 
-        struct Word * word = (struct Word *) malloc(sizeof(Word));
-        word -> index = n;
-        word -> next = wordListPtr;
+        	Word * word = (Word *) malloc(sizeof(Word));
+        	word -> index = n;
+	        word -> next = wordListPtr;
 
-        int i = 0;
-        for(char ch = fgetc(filePtr); ch != ' ';ch = fgetc(filePtr), i++)
-        {
-            word_1[i] = ch;
-        }
-        word_1[i+1] = '\0';
-        word -> enWord = word_1;
+	        int i = 0;
+	        for(char ch = fgetc(file); ch != ' ';ch = fgetc(file), i++)
+	        {	
+	            word_1[i] = ch;
+	        }
+	        word_1[i+1] = '\0';
+	        word -> enWord = word_1;
 
-        i = 0;
-        for(char ch = fgetc(filePtr); ch != '\n';ch = fgetc(filePtr), i++)
-        {
-            word_2[i] = ch;
-        }
-        word_1[i+1] = '\0';
-        word -> ruWord = word_2;
-        
-        wordListPtr = word;
-    }
+	        i = 0;
+	        for(char ch = fgetc(file); ch != '\n';ch = fgetc(file), i++)
+	        {
+			word_2[i] = ch;
+	        }
+	        word_1[i+1] = '\0';
+	        word -> ruWord = word_2;
+		
+	        wordListPtr = word;
+	}
 
-    return wordListPtr;
-};
+    	return wordListPtr;
+}
+
